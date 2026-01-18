f6f357f410dc2c28558a21710b12509401bd501b
lovepreet49213-max <lovepreet49213@gmail.com>
2026-01-19 00:15:49 +0530

    updateddirectory

---
+++ api/index.js
@@ -0,0 +1,53 @@
+﻿
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+
+import cors from "cors";
+
+import transporter from "./src/component/nodemailer.js";
+
+
+const app = express();
+
+app.use(cors({
+    origin: "https://www.eaglesonproperty.com",
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+}));
+
+app.options("*", cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Backend is running 🚀");
+});
+
+
+// POST endpoint to send email
+app.post("/send-email", async (req, res) => {
+    const { to, subject, text } = req.body;
+
+    try {
+
+        console.log(transporter);
+        await transporter.sendMail({
+            from: 'Resume Service<' + process.env.EMAIL_USER + '>',
+            to: to,
+            subject: subject,
+            text: text,
+        });
+
+        res.status(200).json({ message: "Email sent successfully!" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Failed to send email", error });
+    }
+});
+
+
+
+export default app;
+
+
